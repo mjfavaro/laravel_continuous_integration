0cd45908134a04dafc3a8c217ff171efa550da53
Maurício Fávaro <mjfavaro@gmail.com>
2014-12-11 19:16:18 -0200

    Primeira Publicacao

---
+++ app/config/testing/database.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'pgsql' => array(
+			'driver'   => 'pgsql',
+			'host'     => 'localhost',
+			'database' => 'laravel_continuous_integration',
+			'username' => 'postgres',
+			'password' => 'secret',
+			'charset'  => 'utf8',
+			'prefix'   => '',
+			'schema'   => 'public',
+		),
+
+	),
+
+);
